11d518bd48768e53af78ae8ff723b21756eb27ef
Nathan Hammond <github.com@nathanhammond.com>
2016-11-05 02:12:38 -0500

    Fix benchmarks.

--- bench/benches/handlers-for.js
+++ bench/benches/handlers-for.js
@@ -4,12 +4,12 @@ var router = new RouteRecognizer();
 var i = 1000;
 
 while (i--) {
-  router.add([{ path: "/foo/" + i, handler: { handler: i } }], { as: 'foo'});
+  router.add([{ path: "/foo/" + i, handler: { handler: i } }], { as: 'foo'+i });
 }
 
 module.exports = {
   name: 'Handlers For',
   fn: function() {
-    router.handlersFor('foo');
+    router.handlersFor('foo1');
   }
 };
